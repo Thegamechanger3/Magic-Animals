a4dbbe55f03d777ab5e0652d3a6e53b897afdb63
Thegamechanger3 <luxentislu@gmail.com>
2026-02-26 09:09:32 +0100

    Update client.js

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
+    name: "Magic Animals",
     email: "help@codestitch.app",
     phoneForTel: "555-779-4407",
     phoneFormatted: "(555) 779-4407",
